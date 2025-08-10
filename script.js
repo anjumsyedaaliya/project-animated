3c003aa19675a9da3f6e3df6de8b80b641d5bd3f
anjumsyed <786anjumsyed@gmail.com>
2025-08-10 12:51:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// eslint-disable-next-line no-unused-vars
+function showRegister() {
+    document.getElementById("login").style.display = "none";
+    document.getElementById("register").style.display = "block";
+}
